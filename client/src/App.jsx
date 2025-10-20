8f6a5b5fad96343a2002dde1151adc4d75e9e8e7
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2025-10-20 08:51:42 -0700

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-export default function App() {
-  return (
-    <div className='text-red-400'>
-      app
-    </div>
+// Sayfa bileşenlerini import et (örnek)
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
+// Rotaları tanımla
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+    </>
   )
+);
+
+// Uygulama
+export default function App() {
+  return <RouterProvider router={router} />;
 }

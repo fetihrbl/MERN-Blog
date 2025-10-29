c1bfa990ebc1edd18063d5040757135984c012fb
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2025-10-29 01:42:57 -0700

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 
 function Layout() {
@@ -34,7 +35,9 @@ const router = createBrowserRouter(
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
       <Route path="/projects" element={<Projects />} />
     </Route>
   )

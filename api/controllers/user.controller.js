705e5ea51a622a7130ba28580b91b9ced920d1a6
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2025-10-30 05:44:33 -0700

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -83,3 +83,15 @@ export const updateUser = async (req, res, next) => {
     return next(errorHandler(500, err.message || "Internal Server Error"));
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if(req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error)
+  }
+}
